788225a6531eaaca8f3e66118fa3259fa63d0be8
Danish Shafi <danish.shafi@live.co.uk>
2023-10-02 17:48:41 +0200

    Config files changed, Node updated and Prettier included as dependency

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover and share AI prompts",
+};
+
+const layout = () => {
+  return <div></div>;
+};
+
+export default layout;
